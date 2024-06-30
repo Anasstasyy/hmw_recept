6644da0cf29c4ccc6ec2915e124323a2afc46c93
Anastasia <sim200421@gmail.com>
2024-07-01 01:53:07 +0300

    cheange recept.py

--- recept.py
+++ recept.py
@@ -1,34 +1,33 @@
-cook_book = {}
-with open('cookbook.txt', 'rt', encoding = "utf-8") as file:
-    for l in file:
-        dish = l.strip()
-        ingredient_list = []
-        count = file.readline()
-        for i in range(int(count)):
-            dish = file.readline()
-            ingredient_name, quantity, measure = dish.strip().split(' | ')
-            ingredient_list.append({'ingredint_name': ingredient_name, 'quantity': quantity, 'measure': measure})
-            dep = {dish: ingredient_list}
-        separstse = file.readline()
-        cook_book.update(dep)
-        
-#print(f'cook_book = {cook_book}')
-        
-        
+with open('cookbook.txt', encoding='utf-8') as file:
+    cook_book = {}
+    for i in file:
+        recepie_name = i.strip()
+        ingredients_count = file.readline()
+        ingredients = []
+        for p in range(int(ingredients_count)):
+            recepie = file.readline().strip().split(' | ')
+            product, quantity, word = recepie
+            ingredients.append({'product': product, 'quantity': quantity, 'measure': word})
+        file.readline()
+    cook_book[recepie_name] = ingredients
+    
+    
+    # print(f'cook_book = {cook_book}')
+    
 def get_shop_list_by_dishes(dishes, person_count):
     result ={}
     for dish in dishes:
         if dish in cook_book:
             for consist in cook_book[dish]:
-                if consist['product'] in result:
-                    result[consist['product']]['quantity'] += consist['quantity'] * person_count
-                else:
-                    result[consist['product']] = {'measure': consist['measure'], 'quantity': (consist['quantity'] * person_count)}
+                result ({'measure': cook_book['measure'],'quantity': (int(cook_book['quantity'])*int(person_count))})
         else:
-            print('ошибка')
-    print(result)
+            print(f'Блюда "{dish}" нет в книге рецептов')
+    return result
+                
+
 get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
 
+    
 
 
     
