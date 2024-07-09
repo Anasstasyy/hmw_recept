7c9422d00b4d0c3eea7d21350867c5011dd7bd96
Anastasia <sim200421@gmail.com>
2024-07-10 01:47:55 +0300

    change 4

--- recept.py
+++ recept.py
@@ -6,35 +6,31 @@ with open('cookbook.txt', encoding='utf-8') as file:
     for k in file:
         ingredients = []
         recepie_name = k.strip()
-        # print(recepie_name)
         ingredients_count = file.readline()
         cook_book[recepie_name] = ingredients
-        # print(ingredients_count)
-
+    
         for p in range(int(ingredients_count)):
             recepie = file.readline().strip().split(' | ')
             product, quantity, weight = recepie
             ingredients.append({'ingredient_name': product, 'quantity': quantity, 'measure': weight})
         file.readline()
 
-    # print(cook_book)
-    # print(ingredients)
     
-    def get_shop_list_by_dishes(dishes, person_count):
-        result ={}
-        for dish in dishes:
-            if dish in cook_book:
-                for ingredient in cook_book[dish]:
-                    ingredient_name = ingredient['ingredient_name']
-                    measure = ingredient['measure']
-                    quantity = ingredient['quantity'] * person_count
-                    if ingredient_name in result:
-                        result[ingredient_name]['quantity'] += quantity
-                    else:
-                        result[ingredient_name] = {'measure': measure, 'quantity': quantity}
-        for ingredient, values in result.items():
-            print(f"  '{ingredient}': {values},")
-    get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+def get_shop_list_by_dishes(dishes, person_count: int):
+    result ={}
+    for dish in dishes:
+        if dish in cook_book:
+            for ingredient in cook_book[dish]:
+                ingredient_name = ingredient['ingredient_name']
+                measure = ingredient['measure']
+                quantity = ingredient['quantity'] * int(person_count) 
+                if ingredient_name in result:
+                    result[ingredient_name]['quantity'] += quantity
+                else:
+                    result[ingredient_name] = {'measure': measure, 'quantity': quantity}
+    for ingredient, values in result.items():
+        print(f"  '{ingredient}': {values},")
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'],2)
                     
 
 
